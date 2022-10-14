99a0ae87cc74b2d0d1707ed1394a1661e2f9f70b
AnisTohme-961 <anistohme1@hotmail.com>
2022-10-14 21:19:58 +0300

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import dbConnection from "./config/mongoconnect.js"
 
 dotenv.config()
 
@@ -8,3 +9,16 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json())
+
+app.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "This is the Car Management System API",
+    author: "Anis Tohme",
+    repository: "https://github.com/AnisTohme-961/Cars.git",
+  })
+})
+
+app.listen(PORT, async () => {
+  await dbConnection()
+  console.log(`Server is running on port ${PORT}`)
+})
