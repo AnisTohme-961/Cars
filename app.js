aae870ab3c9f46efe878e8a2cec8c45abca658bb
AnisTohme-961 <anistohme1@hotmail.com>
2022-10-14 22:35:25 +0300

    app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import dbConnection from "./config/mongoconnect.js"
 import ErrorHandler from "./Middleware/ErrorHandler.js"
+import authRoutes from "./routes/auth.routes.js"
 
 dotenv.config()
 
