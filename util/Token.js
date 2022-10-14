d23c9a78a69a83c0d35ab863744cf856be059369
AnisTohme-961 <anistohme1@hotmail.com>
2022-10-14 21:18:20 +0300

    Token Utility

---
+++ util/Token.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken"
+
+export const generateToken = (user) => {
+  const { _id, email } = user
+  const accessToken = jwt.sign({ id: _id, email }, process.env.JWT_SECRET, {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN,
+  })
+
+  const refreshToken = jwt.sign({ id: _id, email }, process.env.JWT_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN,
+  })
+
+  return { accessToken, refreshToken }
+}
