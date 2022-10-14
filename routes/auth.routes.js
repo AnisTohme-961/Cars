fde69a75274350505f3f060195e5270389b7ce2d
AnisTohme-961 <anistohme1@hotmail.com>
2022-10-14 22:35:01 +0300

    Auth Routes

---
+++ routes/auth.routes.js
@@ -0,0 +1,16 @@
+import express from "express"
+import { login, signUp } from "../controllers/auth"
+
+const router = express.Router()
+
+// @route   POST /auth/signup
+// @desc    User sign up
+// @access  Private
+
+router.post("/signup", signUp)
+
+// @route   POST /auth/login
+// @desc    User login
+// @access  Private
+
+router.post("/login", login)
