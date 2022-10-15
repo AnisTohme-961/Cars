07bfaf7b0544502538fd798a27e93739116f4163
AnisTohme-961 <anistohme1@hotmail.com>
2022-10-15 16:50:54 +0300

    Index Validator

--- Validations/index.js
+++ Validations/index.js
@@ -1,11 +1,15 @@
 import user from "./user.validator.js"
 import signup from "./signup.validator.js"
 import login from "./login.validator.js"
+import car from "./car.validator.js"
+import category from "./category.validator.js"
 
 const Validators = {
     user,
     login,
-    signup
+    signup,
+    car,
+    category
 }
 
 export default Validators
