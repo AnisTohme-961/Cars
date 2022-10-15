3ad8706754e975054bf532a18fc26d3f676d5484
AnisTohme-961 <anistohme1@hotmail.com>
2022-10-15 12:23:59 +0300

    Category Model

---
+++ model/category.js
@@ -0,0 +1,16 @@
+import mongoose, { mongo, Schema } from "mongoose"
+
+const categorySchema = mongoose.Schema(
+  {
+    categoryName: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
