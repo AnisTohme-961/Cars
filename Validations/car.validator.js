279382ab715e8c1768063329725ea98bd1f92ea5
AnisTohme-961 <anistohme1@hotmail.com>
2022-10-15 12:23:32 +0300

    Car Validator

---
+++ Validations/car.validator.js
@@ -0,0 +1,11 @@
+import Joi from "joi"
+import JoiObjectId from "joi-objectid"
+
+const myJoiObjectId = JoiObjectId(Joi)
+
+const carSchema = Joi.object({
+  carName: Joi.string().required().min(3).max(30),
+  carImage: Joi.string().uri().required(),
+  categoryId: myJoiObjectId("Category"),
+  tags: Joi.array().items(Joi.string()),
+})
