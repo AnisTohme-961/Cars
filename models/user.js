4a79b021b39dc7efffbfb24ac374883ca76ae35a
AnisTohme-961 <anistohme1@hotmail.com>
2022-10-15 16:52:28 +0300

    User Model

---
+++ models/user.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose"
+
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "First Name is required"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+    cars: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Car",
+      },
+    ],
+    categories: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Category",
+      },
+    ],
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model("User", userSchema)
