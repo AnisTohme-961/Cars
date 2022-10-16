eb2c0536fde4e0cff92613639a0fb8b5300738a1
AnisTohme-961 <anistohme1@hotmail.com>
2022-10-16 18:55:56 +0300

    User Controller

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -4,7 +4,7 @@ import createError from "../util/Error.js"
 
 export const getUsers = async (req, res, next) => {
   try {
-    const users = await User.find().select("-password -token -createdAt -updatedAt")
+    const users = await User.find().select("-password -token -createdAt -updatedAt").sort({ createdAt: -1 })
     if (users.length <= 0) {
       return next(createError("Users not found", 404))
     }
