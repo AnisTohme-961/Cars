cab2c2aee033f59eb54fd852106d833e8866c31f
AnisTohme-961 <anistohme1@hotmail.com>
2022-10-17 16:23:56 +0300

    App.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 app.get("/", (req, res, next) => {
   res.status(200).json({
-    message: "This is the Car Management System API",
+    message: "Welcome! This is the Car Management System API",
     author: "Anis Tohme",
     repository: "https://github.com/AnisTohme-961/Cars.git",
   })
