18798336137f727e8cc1ba9ef629c21f60fdefe7
AnisTohme-961 <anistohme1@hotmail.com>
2022-11-02 16:09:36 +0200

    Password Validator

--- Validations/password.validator.js
+++ Validations/password.validator.js
@@ -1,6 +1,6 @@
 import Joi from "joi"
 
-const passwordValidator = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$@^%&? "])[a-zA-Z0-9!#$@^%&?]{8,20}$/
+const passwordValidator = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,20}$/
 
 const passwordSchema = Joi.object({
   oldPassword: Joi.string().required().pattern(passwordValidator),
