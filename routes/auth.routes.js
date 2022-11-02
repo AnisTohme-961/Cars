9b7030e74a0fe5eedf5c58d68a18a1666017e611
AnisTohme-961 <anistohme1@hotmail.com>
2022-11-02 14:52:37 +0200

    Auth Routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { login, signUp } from "../controllers/auth.js"
+import Validator from "../Middleware/Validator.js"
 
 const router = express.Router()
 
@@ -7,12 +8,12 @@ const router = express.Router()
 // @desc    User sign up
 // @access  Private
 
-router.post("/signup", signUp)
+router.post("/signup", Validator("signup"), signUp)
 
 // @route   POST /auth/login
 // @desc    User login
 // @access  Private
 
-router.post("/login", login)
+router.post("/login", Validator("login"), login)
 
 export default router
