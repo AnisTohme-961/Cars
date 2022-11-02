192c20ee24eea82bc143de72432c1077d8ae3c1e
AnisTohme-961 <anistohme1@hotmail.com>
2022-11-02 16:09:13 +0200

    Login Validator

--- Validations/login.validator.js
+++ Validations/login.validator.js
@@ -1,6 +1,6 @@
 import Joi from "joi"
 
-const passwordValidator = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$@^%&? "])[a-zA-Z0-9!#$@^%&?]{8,20}$/
+const passwordValidator = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,20}$/
 
 const loginSchema = Joi.object({
   email: Joi.string().email().required(),
